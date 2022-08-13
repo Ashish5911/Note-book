46bbb6fb7ec22b2c0b97547f711eb792513aabc1
ashish kumar <ak735088@gmail.com>
2022-08-13 20:09:14 +0530

    console host

--- src/components/Login.js
+++ src/components/Login.js
@@ -5,7 +5,7 @@ import { useNavigate } from "react-router-dom";
 const Login = (props) => {
   // let host=process.env.REACT_APP_HOST || "http://localhost:4000";
   let host=process.env.NODE_ENV === 'production' ? 'https://inote-book1.herokuapp.com' : 'http://localhost:4000';
-  
+  console.log(host);
   const [credentials, setCredentials] = useState({ email: "", password: "" });
   let navigate = useNavigate();
 
