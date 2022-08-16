ee43556d416cc6b5955607681646f2b0d33c643e
ashish kumar <ak735088@gmail.com>
2022-08-16 11:36:44 +0530

    new changes including proc

---
+++ backend/nitish18.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void display();
+int main(){
+    int a;
+    for
+    printf("my name is nitish\n");
+    display();
+    printf("India is my country");
+}
+void display()
+{
+    printf("i am fron punjab/n");
+}
